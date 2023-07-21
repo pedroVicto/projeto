b62ef423ffe07d80753fbacbc4e343041ff9e1b5
pedroVicto <victorpedrosilva445@gmail.com>
2023-07-21 15:29:50 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+   
+    //pegar a tag img
+    const img = document.querySelector('#profile img')
+    //substituir a imgagem
+    if(html.classList.contains('light')) {
+    //se tiver light mode adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+    }
+    
+    //se não deixar na imagem normal
+
+
+}
